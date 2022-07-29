2cbdd08af2e644eb4e0dd73a8e8362adeb2f8d9f
Jake <jakedves@gmail.com>
2022-07-29 22:25:44 +0100

    Initial Commit

---
+++ Visionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Visionary
+//
+//  Created by Jake Davies on 29/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Visionary/VisionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionaryApp.swift
+//  Visionary
+//
+//  Created by Jake Davies on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
